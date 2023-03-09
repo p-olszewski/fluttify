f8aa7f28f326fa3f28e4bc2209eea59e11d3e486
p-olszewski <przemo1297@gmail.com>
2023-03-09 21:59:59 +0100

    add signIn method to button

--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttify/net/auth.dart';
+import 'package:fluttify/ui/home/home.dart';
 import 'package:fluttify/ui/registration/registration.dart';
 import 'package:fluttify/ui/shared/shared.dart';
 
@@ -10,8 +13,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final TextEditingController _emailField = TextEditingController();
-  final TextEditingController _passwordField = TextEditingController();
+  final TextEditingController _emailFieldController = TextEditingController();
+  final TextEditingController _passwordFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +34,24 @@ class _LoginState extends State<Login> {
               style: TextStyle(color: Colors.white, fontSize: 32),
             ),
             SizedBox(height: screenHeight / 15),
-            CustomTextFormField(controller: _emailField, labelText: "Email", hintText: "youremail@email.com"),
+            CustomTextFormField(controller: _emailFieldController, labelText: "Email", hintText: "youremail@email.com"),
             SizedBox(height: screenHeight / 100),
-            CustomTextFormField(controller: _passwordField, labelText: "Password", hintText: "password"),
+            CustomTextFormField(controller: _passwordFieldController, labelText: "Password", hintText: "password"),
             SizedBox(height: screenHeight / 15),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                bool shouldRedirect = await signIn(_emailFieldController.text, _passwordFieldController.text);
+                if (shouldRedirect) {
+                  Fluttertoast.showToast(msg: "Logged in");
+                  if (!mounted) return;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                }
+              },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.white,
               ),
