583c6636ce99a3159d66d771078398ba8c5e3bcf
p-olszewski <przemo1297@gmail.com>
2023-03-10 19:11:17 +0100

    merge auth page refactoring

--- lib/ui/login/login.dart
+++ lib/ui/login/login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:fluttify/net/auth.dart';
 import 'package:fluttify/ui/home/home.dart';
-import 'package:fluttify/ui/registration/registration.dart';
 import 'package:fluttify/ui/shared/shared.dart';
 
 class Login extends StatefulWidget {
@@ -16,7 +15,7 @@ class _LoginState extends State<Login> {
   final TextEditingController _emailFieldController = TextEditingController();
   final TextEditingController _passwordFieldController = TextEditingController();
   final TextEditingController _repeatedPasswordFieldController = TextEditingController();
-  bool isLoginView = true;
+  bool isLoginPage = true;
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +31,7 @@ class _LoginState extends State<Login> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              isLoginView ? "Sign in to fluttify" : "Create Account",
+              isLoginPage ? "Sign in to fluttify" : "Create Account",
               style: const TextStyle(color: Colors.white, fontSize: 32),
             ),
             SizedBox(height: screenHeight / 15),
@@ -41,14 +40,14 @@ class _LoginState extends State<Login> {
             CustomTextFormField(controller: _passwordFieldController, labelText: "Password", hintText: "password", obscure: true),
             SizedBox(height: screenHeight / 100),
             Visibility(
-              visible: !isLoginView,
+              visible: !isLoginPage,
               child: CustomTextFormField(
                   controller: _repeatedPasswordFieldController, labelText: "Repeat password", hintText: "password", obscure: true),
             ),
             SizedBox(height: screenHeight / 15),
             ElevatedButton(
               onPressed: () async {
-                bool shouldRedirect = isLoginView
+                bool shouldRedirect = isLoginPage
                     ? await signIn(_emailFieldController.text, _passwordFieldController.text)
                     : await signUp(
                         _emailFieldController.text,
@@ -57,7 +56,7 @@ class _LoginState extends State<Login> {
                       );
                 if (shouldRedirect) {
                   Fluttertoast.showToast(
-                    msg: isLoginView ? "Logged in" : "Account created",
+                    msg: isLoginPage ? "Logged in" : "Account created",
                   );
                   if (!mounted) return;
                   Navigator.push(
@@ -72,18 +71,20 @@ class _LoginState extends State<Login> {
                 backgroundColor: Colors.white,
               ),
               child: Text(
-                isLoginView ? "Sign in" : "Register and login",
+                isLoginPage ? "Sign in" : "Register and login",
                 style: const TextStyle(color: Colors.indigo),
               ),
             ),
             TextButton(
-              onPressed: () async {
-                setState(() {
-                  isLoginView = !isLoginView;
-                });
-              },
+              onPressed: () => setState(() {
+                isLoginPage = !isLoginPage;
+                _emailFieldController.clear();
+                _passwordFieldController.clear();
+                _repeatedPasswordFieldController.clear();
+                FocusScope.of(context).unfocus();
+              }),
               child: Text(
-                isLoginView ? "go to registration page" : "go back to the login page",
+                isLoginPage ? "go to registration page" : "go back to the login page",
                 style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
               ),
             ),
