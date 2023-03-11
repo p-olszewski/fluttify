b0acdc3f2fee223d663e02d101da6a1babc09361
p-olszewski <przemo1297@gmail.com>
2023-03-11 10:35:47 +0100

    change isLoginpage to local

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -13,9 +13,11 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final TextEditingController _emailFieldController = TextEditingController();
-  final TextEditingController _passwordFieldController = TextEditingController();
-  final TextEditingController _repeatedPasswordFieldController = TextEditingController();
-  bool isLoginPage = true;
+  final TextEditingController _passwordFieldController =
+      TextEditingController();
+  final TextEditingController _repeatedPasswordFieldController =
+      TextEditingController();
+  bool _isLoginPage = true;
 
   @override
   Widget build(BuildContext context) {
@@ -31,24 +33,36 @@ class _LoginState extends State<Login> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              isLoginPage ? "Sign in to fluttify" : "Create Account",
+              _isLoginPage ? "Sign in to fluttify" : "Create Account",
               style: const TextStyle(color: Colors.white, fontSize: 32),
             ),
             SizedBox(height: screenHeight / 15),
-            CustomTextFormField(controller: _emailFieldController, labelText: "Email", hintText: "youremail@email.com", obscure: false),
+            CustomTextFormField(
+                controller: _emailFieldController,
+                labelText: "Email",
+                hintText: "youremail@email.com",
+                obscure: false),
             SizedBox(height: screenHeight / 100),
-            CustomTextFormField(controller: _passwordFieldController, labelText: "Password", hintText: "password", obscure: true),
+            CustomTextFormField(
+                controller: _passwordFieldController,
+                labelText: "Password",
+                hintText: "password",
+                obscure: true),
             SizedBox(height: screenHeight / 100),
             Visibility(
-              visible: !isLoginPage,
+              visible: !_isLoginPage,
               child: CustomTextFormField(
-                  controller: _repeatedPasswordFieldController, labelText: "Repeat password", hintText: "password", obscure: true),
+                  controller: _repeatedPasswordFieldController,
+                  labelText: "Repeat password",
+                  hintText: "password",
+                  obscure: true),
             ),
             SizedBox(height: screenHeight / 15),
             ElevatedButton(
               onPressed: () async {
-                bool shouldRedirect = isLoginPage
-                    ? await signIn(_emailFieldController.text, _passwordFieldController.text)
+                bool shouldRedirect = _isLoginPage
+                    ? await signIn(_emailFieldController.text,
+                        _passwordFieldController.text)
                     : await signUp(
                         _emailFieldController.text,
                         _passwordFieldController.text,
@@ -56,7 +70,7 @@ class _LoginState extends State<Login> {
                       );
                 if (shouldRedirect) {
                   Fluttertoast.showToast(
-                    msg: isLoginPage ? "Logged in" : "Account created",
+                    msg: _isLoginPage ? "Logged in" : "Account created",
                   );
                   if (!mounted) return;
                   Navigator.push(
@@ -71,21 +85,24 @@ class _LoginState extends State<Login> {
                 backgroundColor: Colors.white,
               ),
               child: Text(
-                isLoginPage ? "Sign in" : "Register and login",
+                _isLoginPage ? "Sign in" : "Register and login",
                 style: const TextStyle(color: Colors.indigo),
               ),
             ),
             TextButton(
               onPressed: () => setState(() {
-                isLoginPage = !isLoginPage;
+                _isLoginPage = !_isLoginPage;
                 _emailFieldController.clear();
                 _passwordFieldController.clear();
                 _repeatedPasswordFieldController.clear();
                 FocusScope.of(context).unfocus();
               }),
               child: Text(
-                isLoginPage ? "go to registration page" : "go back to the login page",
-                style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
+                _isLoginPage
+                    ? "go to registration page"
+                    : "go back to the login page",
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w400),
               ),
             ),
           ],
