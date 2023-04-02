05abab86c4747308ff99654c967073880025ab12
Oxozon <oxozon.o4@gmail.com>
2023-04-02 13:44:18 +0200

    add init add & remove user access to list

--- lib/details/details.dart
+++ lib/details/details.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:fluttify/services/firestore.dart';
 import 'package:fluttify/shared/shared.dart';
 
@@ -16,6 +17,7 @@ class _DetailsState extends State<Details> {
   late Stream<QuerySnapshot<Map<String, dynamic>>> snapshot;
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _addUserController = TextEditingController();
 
   @override
   void initState() {
@@ -39,6 +41,73 @@ class _DetailsState extends State<Details> {
       appBar: AppBar(
         title: Text(widget.listTitle),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: const Text('Zarządzaj użytkownikami'),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Chip(
+                            label: const Text('tomasz.baltonowski@test.com'),
+                            backgroundColor: Colors.green,
+                            onDeleted: () {
+                              Fluttertoast.showToast(
+                                msg: 'Usunięto użytkownika z listy',
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                                timeInSecForIosWeb: 1,
+                                fontSize: 16.0,
+                              );
+                            },
+                          ),
+                          Chip(
+                            label: const Text('krzysztof.kolumb@test.com'),
+                            backgroundColor: Colors.blue,
+                            onDeleted: () {
+                              Fluttertoast.showToast(
+                                msg: 'Usunięto użytkownika z listy',
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                                timeInSecForIosWeb: 1,
+                                fontSize: 16.0,
+                              );
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: TextField(
+                              controller: _addUserController,
+                              textAlign: TextAlign.left,
+                              decoration: const InputDecoration(
+                                labelText: 'Email nowego użytkownika',
+                                hintText: 'np. test@test.com',
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          child: const Text('Udostępnij'),
+                          onPressed: () {
+                            _addUserController.clear();
+                            Fluttertoast.showToast(
+                              msg: 'Udostępniono listę',
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              fontSize: 16.0,
+                            );
+                          },
+                        ),
+                      ],
+                    )),
+            icon: const Icon(Icons.manage_accounts),
+          ),
+        ],
       ),
       body: SizedBox(
         width: screenWidth,
